8c009a584946c768257531af7fea91c67b1cbead
Benjamin Andres Rebolledo Rebolledo <105505096+Akxlarre@users.noreply.github.com>
2025-06-25 00:09:32 -0400

    Refine splash screen logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SplashScreenComponent } from './splash-screen/splash-screen';
 import { ThemeService } from './theme/services/theme.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SplashScreenComponent],
   templateUrl: './app.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  showSplash = true;
+
   constructor(private theme: ThemeService) {
     this.theme.loadSavedTheme();
+  }
+
+  ngOnInit() {
+    setTimeout(() => this.hideSplash(), 2000);
+  }
+
+  hideSplash() {
+    const splash = document.querySelector('app-splash-screen');
+    if (splash) {
+      splash.classList.add('fade-out');
+      setTimeout(() => (this.showSplash = false), 700);
+    } else {
+      this.showSplash = false;
+    }
   }
 }
---
+++ src/app/splash-screen/splash-screen.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splash-screen',
+  standalone: true,
+  templateUrl: './splash-screen.html',
+  styleUrls: ['./splash-screen.css'],
+  imports: [],
+})
+export class SplashScreenComponent {}
